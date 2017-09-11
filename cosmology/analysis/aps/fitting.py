841bf6d478395f63428c0282053a78324f779ef5
Damian <EternalTime@users.noreply.github.com>
2017-09-11 18:26:29 -0400

    Changed the fitting module. Added a Lagendre polynoial basis.

--- cosmology/analysis/aps/fitting.py
+++ cosmology/analysis/aps/fitting.py
@@ -2,14 +2,32 @@ import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 
-def nonparametric_fit(data,error,basis,lType = 'NSS',maxJ = 100):
+def basis_cos(j,x):
+    if j == 0:
+        return np.ones(np.shape(x))
+    else:
+        return 1.4142135623730951*np.cos(j*np.pi*x)
+
+def basis_leg(j,x):
+    J = np.zeros(j+1)
+    J[-1] = 1
+    return np.sqrt((2*j+1.))*np.polynomial.legendre.Legendre(J,[0,1])(x)
+
+def npf_makeU(x,basis):
+    N = len(x)
+    U = np.ones([N,N])
+    for i in range(0,N):
+        U[:,i] = basis(i,x)
+    return U/np.sqrt(N)
+
+def nonparametric_fit(data,error,U,lType = 'NSS',JRange = [1,100]):
     """
     ----------------------------------------------------------------------------
-    FUNCTION:       dic = nonparametric_fit(data, error, basis,[lType, maxJ])
+    FUNCTION:       dic = nonparametric_fit(data, error, basis,[lType, JRange])
     ----------------------------------------------------------------------------
     INPUT:          data    (array)
                     error   (array)
-                    basis   (function: arguments(j,x))
+                    U       (array) orthogonal matrix made from basis
     ----------------------------------------------------------------------------
     OUTPUT:         dic     (dictionary, keys: nbf, Risk, EDoF)
                     Outputs a dictionary containing the nonparametric best fit
@@ -18,6 +36,8 @@ def nonparametric_fit(data,error,basis,lType = 'NSS',maxJ = 100):
                     freedom (EDoF). The algorithm follows 1107.0516v2.
     ----------------------------------------------------------------------------
     """
+    minJ = JRange[0]
+    maxJ = JRange[1]
     N = len(data)
     Y = data
     E = np.diag(1/error**2) #initialize inverse variance
@@ -25,13 +45,6 @@ def nonparametric_fit(data,error,basis,lType = 'NSS',maxJ = 100):
     # until we get that we'll settle for just the variance and leave the off-
     # diagonal terms = 0
     x = (2.*np.array(list(range(N)))+1.)/(2.*N)
-    # Make the Unitary matrix from the basis. if the basis is wrong then the
-    # resulting matrix will not be orthogonal and the algorithm will output
-    # bullshit. TODO: add a check to ensure U is orthogonal
-    U = np.ones([N,N])
-    for i in range(1,N):
-        U[:,i] = basis(i,x)
-    U = U/np.sqrt(N)
     #Use the U matrix in a few places to make the B, W, and Z matrices
     # (see the paper if this is confusing)
     B = np.dot(np.dot(np.transpose(U),B),U)
@@ -43,25 +56,18 @@ def nonparametric_fit(data,error,basis,lType = 'NSS',maxJ = 100):
     print('\n---------------- Calculating Risk ---------------')
     if lType in ['NSS']:
         print('Shrinkage Method:         Nested Subset Selection\n')
-        R = np.zeros(maxJ)
+        R = np.zeros(maxJ-minJ+1)
         EDoF = np.zeros(np.shape(R))
 
-        for j in tqdm.tqdm(range(1,maxJ+1)):
+        for j in tqdm.tqdm(range(minJ,maxJ+1)):
             D = np.diag([1]*j+[0]*(N-j))
             Db = np.eye(N)-D
             temp = [np.dot(np.dot(np.dot(np.dot(np.transpose(Z),Db),W),Db),Z),
                     np.trace(np.dot(np.dot(np.dot(D,W),D),B)),
                     -np.trace(np.dot(np.dot(np.dot(Db,W),Db),B))]
-            R[j-1] = sum(temp)
-            EDoF[j-1] = sum(np.diagonal(D))
-        J = list(R).index(min(R[1::]))+1
+            R[j-minJ] = sum(temp)
+            EDoF[j-minJ] = j#sum(np.diagonal(D))
+        J = list(R).index(min(R[1::]))+minJ
 
         print('\nNPfit optimized at J = ' + str(J))
         return {'nbf':np.sqrt(N)*np.dot(U,([1]*j+[0]*(N-j))*Z),'Risk':R,'EDoF':EDoF}
-
-
-def basis_cos(j,x):
-    if j == 0:
-        return np.ones(np.shape(x))
-    else:
-        return 1.4142135623730951*np.cos(j*np.pi*x)
