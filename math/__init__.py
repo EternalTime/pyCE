4fa3f8b8f6837a1279f27f057627c185b124ba73
Damian <EternalTime@users.noreply.github.com>
2018-02-22 17:31:04 -0500

    Added a fourier matrix to the osc class to implement faster fourier transforms at any timestep.

--- math/__init__.py
+++ math/__init__.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.special as sp
+from numpy.matlib import repmat
 
 def radialFT(d,f,r):
     k = np.array(range(5*len(r)))*np.pi/(10*r[-1])
@@ -14,6 +15,24 @@ def radialFT(d,f,r):
     #normalizes to ensure Plancheral's theorem holds
     ft = ft*np.sqrt(radial_integrate(r,np.abs(f)**2,d)/radial_integrate(k,np.abs(ft)**2,d))
     return ft,k
+
+def radialFT_mat(d,r):
+    k = np.array(range(5*len(r)))*np.pi/(10*r[-1])
+    F = np.zeros([len(k),len(r)])
+    dr0 = [np.diff(r)[0]/2]
+    if d>1:
+        a           = float(d)/2.0
+        F[1:,:]     = (np.sqrt(np.pi)*(2.0**(a-2))
+                        *repmat(k[1:]**(1-a),len(r),1).T
+                        *(sp.jv(a-1,np.outer(k[1:],r))*(r**a))
+                        *repmat(np.array(dr0+list(np.diff(r))),len(k)-1,1))
+        F0          = np.eye(len(k))
+        F0[0,0:8]   = np.array([0, 287/48.0, -(61/4.0), 1033/48.0,
+                                -(109/6.0), 147/16.0, -(31/12.0), 5/16.0])
+        F = np.dot(F0,F)
+    else:
+        F = np.cos(np.outer(k,r))
+    return F,k
 
 def sphere_solid_angle(d):
     return 2*np.pi**(d/2.0)/np.math.gamma(d/2.0)
