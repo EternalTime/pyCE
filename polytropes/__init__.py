b4ea958ec6b2d49487dd480b1d0493a1351af2ed
Damian <EternalTime@users.noreply.github.com>
2018-03-30 14:44:36 -0400

    Fixed the polytrope RK4 method to use float64 instead of float128

--- polytropes/__init__.py
+++ polytropes/__init__.py
@@ -30,40 +30,44 @@ class polytrope:
 
     ----------------------------------------------------------------------------
     """
-    def __init__(self,n = 1.5,dr = .001):
+    def __init__(self,n = 1.5,dr = .01):
         self.n = n
         self.gamma = 1+1.0/n
         self.dr = dr
         self.__solve_Lane_Emden__()
+        self.rho = self.theta**self.n
 
     def __solve_Lane_Emden__(self):
         n  = 0
         F1 = lambda r,theta,psi: psi
-        F2 = lambda r,theta,psi: -2*psi/r - theta**self.n
+        F2 = lambda r,theta,psi: -2.0*psi/r - theta**self.n
         theta  = [1.0, 1.0]
-        psi = [-np.finfo(np.float128).eps,-np.finfo(np.float128).eps]
-        r  = [np.finfo(np.float128).eps,self.dr]
+        psi = [-np.finfo(np.float64).eps,-np.finfo(np.float64).eps]
+        r  = [np.finfo(np.float64).eps,self.dr]
         while theta[-1] > 10**-5:
-			try:
-				n   = n + 1
-				k31 = F1(r[-1],theta[-1],psi[-1])
-				k41 = F2(r[-1],theta[-1],psi[-1])
+            try:
+                n   = n + 1
+                k31 = F1(r[-1],theta[-1],psi[-1])
+                k41 = F2(r[-1],theta[-1],psi[-1])
 
-				k32 = F1(r[-1]+.5*self.dr,theta[-1] + .5*self.dr*k31,psi[-1] + .5*self.dr*k41)
-				k42 = F2(r[-1]+.5*self.dr,theta[-1] + .5*self.dr*k31,psi[-1] + .5*self.dr*k41)
+                k32 = F1(r[-1]+.5*self.dr,theta[-1] + .5*self.dr*k31,psi[-1] + .5*self.dr*k41)
+                k42 = F2(r[-1]+.5*self.dr,theta[-1] + .5*self.dr*k31,psi[-1] + .5*self.dr*k41)
 
-				k33 = F1(r[-1]+.5*self.dr,theta[-1] + .5*self.dr*k32,psi[-1] + .5*self.dr*k42)
-				k43 = F2(r[-1]+.5*self.dr,theta[-1] + .5*self.dr*k32,psi[-1] + .5*self.dr*k42)
+                k33 = F1(r[-1]+.5*self.dr,theta[-1] + .5*self.dr*k32,psi[-1] + .5*self.dr*k42)
+                k43 = F2(r[-1]+.5*self.dr,theta[-1] + .5*self.dr*k32,psi[-1] + .5*self.dr*k42)
 
-				k34 = F1(r[-1]+self.dr,theta[-1]+self.dr*k33,psi[-1]+self.dr*k43)
-				k44 = F2(r[-1]+self.dr,theta[-1]+self.dr*k33,psi[-1]+self.dr*k43)
+                k34 = F1(r[-1]+self.dr,theta[-1]+self.dr*k33,psi[-1]+self.dr*k43)
+                k44 = F2(r[-1]+self.dr,theta[-1]+self.dr*k33,psi[-1]+self.dr*k43)
 
-				r.append(r[-1]+self.dr)
-				theta.append(theta[-1] + self.dr/6.*(k31+2.*k32+2.*k33+k34))
-				psi.append(psi[-1] + self.dr/6.*(k41+2.*k42+2.*k43+k44))
-			except OverflowError:
+                dth,dps = (self.dr/6.)*(k31+2.*k32+2.*k33+k34),(self.dr/6.)*(k41+2.*k42+2.*k43+k44)
+                theta.append(theta[-1] + dth)
+                psi.append(psi[-1] + dps)
+                r.append(r[-1]+self.dr)
+
+            except OverflowError:
 				print "Overflow occured at n = ", n
 				break
-        self.theta  = np.array(theta)
-        self.psi = np.array(psi)
-        self.r  = np.array(r)
+        idx = np.array(theta) > 0.0
+        self.theta  = np.array(theta)[idx]
+        self.psi = np.array(psi)[idx]
+        self.r  = np.array(r)[idx]
